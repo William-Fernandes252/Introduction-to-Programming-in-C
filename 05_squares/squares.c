f2a2fcf402edb25f0a249f0146d6744bfd54422f
Coursera Learner <nobody@nowhere.nul>
2020-08-03 15:04:17 +0000

    Finally did it

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -57,7 +57,7 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
       // if so, print a *
-      if(((isInRange(x, x_offset, size2)) && isAtBorder(y, y_offset, (size2 - 1))) || ((isInRange(y, y_offset, size2)) && isAtBorder(x, x_offset, (size2 - 1)))) {
+      if((isInRange(x, x_offset, size2) && isAtBorder(y, y_offset, (size2 - 1))) || (isInRange(y, y_offset, size2) && isAtBorder(x, x_offset, (size2 - 1)))) {
 	printf("*");
       }
       //if not,
@@ -66,7 +66,7 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       // OR
       //    y is less than size1 AND (x is either 0 or size1-1)
       //if so, print a #
-      else if((x < size1 && (y == 0 || y == (size1 - 1))) || (y < size1 && (x == 0 || (size1 - 1)))) {
+      else if((x<size1 && (y==0 || y==(size1-1))) || (y<size1 && (x==0 || x== size1-1))){
 	printf("#");
       } 
       //else print a space
