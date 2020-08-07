5eb5d7188ed801da9f63d6c03b5e6a20ce4feda4
Coursera Learner <nobody@nowhere.nul>
2020-08-07 19:50:21 +0000

    Did

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -6,7 +6,7 @@
 
 void assert_card_valid(card_t c) {
   assert(c.value >= 2 && c.value <= VALUE_ACE);
-  assert(c.suit >= 0 && c.suit <= 4);
+  assert(c.suit >= 0 && c.suit <= 3);
 }
 
 const char * ranking_to_string(hand_ranking_t r) {
