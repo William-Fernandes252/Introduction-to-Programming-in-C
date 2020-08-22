125d1b72e3e07fbaf948bfec600e56da7a50ac51
Coursera Learner <nobody@nowhere.nul>
2020-08-22 03:19:07 +0000

    did

--- 23_power_rec/power.c
+++ 23_power_rec/power.c
@@ -3,8 +3,12 @@
 
 unsigned power (unsigned x, unsigned y) {
 
-  if((x == 0 && y == 0) || y == 1) {
+  if((x == 0 && y == 0)) {
     return 1;
+  }
+
+  if(y == 1) {
+    return x;
   }
 
   if(x == 0) {
