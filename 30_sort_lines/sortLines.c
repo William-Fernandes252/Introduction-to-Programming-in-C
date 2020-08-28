8495a25db34dfe5b6c8eadd274397f6e429ca7d0
Coursera Learner <nobody@nowhere.nul>
2020-08-28 00:28:32 +0000

    done

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -17,8 +17,66 @@ void sortData(char ** data, size_t count) {
 }
 
 int main(int argc, char ** argv) {
+  char ** lines = NULL;
+  char * curr = NULL;
+  size_t sz;
+  size_t i = 0;
+  size_t k = 0; 
+  size_t files = 1;
+  if(argc == 1) {
+    while(getline(&curr, &sz, stdin) >= 0) {
+      lines = realloc(lines, (i+1) * sizeof(*lines));
+      lines[i] = curr;
+      curr = NULL;
+      i++;
+    }
+
+    free(curr);
+
+    sortData(lines, i);
+
+    for(size_t j = 0; j < i; j++) {
+      printf("%s", lines[j]);
+      free(lines[j]);
+    }
+    free(lines);
+  }
+
+  if(argc > 1) {
+    while(argc > k) {
+      FILE * f = fopen(argv[files], "r");
+      if(f == NULL) {
+	printf("Failed to open one of the %s input file.\n", argv[files]);
+	return EXIT_FAILURE;
+      }
+      
+      while(getline(&curr, &sz, f) >= 0) {
+	lines = realloc(lines, (i+1) * sizeof(*lines));
+	lines[i] = curr;
+	curr = NULL;
+	i++;
+      }
+
+      free(curr);
+
+      sortData(lines, i);
+
+      for(size_t j = 0; j < i; j++) {
+	printf("%s", lines[j]);
+	free(lines[j]);
+      }
+      free(lines);
+      if (fclose(f) != 0) {
+	printf("Failed to close one of the input files. Data was lost.\n");
+	return EXIT_FAILURE;
+      }
+
+      k++;
+      files++;
+    }
+
+    printf("All files was read and sorted!");
   
-  //WRITE YOUR CODE HERE!
-  
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
+  }
 }
